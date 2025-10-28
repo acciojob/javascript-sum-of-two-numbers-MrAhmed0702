96d8262a97d755b685bbc20e9dd11cf898818dca
Ahmed Mohammed Jayanti Mochi <amochi670@rku.ac.in>
2025-10-28 23:57:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ let input1 = prompt("Enter the first number:");
 let input2 = prompt("Enter the second number:");
 
 // Write your code here and print the output using alert function
+if(Number(input1) && Number(input2)){
+	alert(`The sum of ${input1} and ${input2} is ${input1 + input2}.`);
+} else {
+	alert(`Invalid input. Please enter a valid number.`)
+}
